93f52dca6c33c8f926c57e8991e0f2f856c49470
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-15 15:33:41 +0100

    modify ada

--- algos/supervised_learning/adaboost.py
+++ algos/supervised_learning/adaboost.py
@@ -6,3 +6,8 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+from algos.utils.data_manipulation import train_test_split
+from algos.utils.data_operation import accuracy_score
+from algos.utils.plot import Plot
+
