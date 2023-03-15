3d52da72149ac4a1c3f642d5083b3808d7152c0a
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-15 15:15:49 +0100

    add preprocessing

--- algos/utils/data_manipulation.py
+++ algos/utils/data_manipulation.py
@@ -4,6 +4,18 @@ import numpy as np
 import math
 import sys
 
+
+
+def shuffle_data(X, y, seed=None):
+    """ Shuffle aléatoire des samples de X et de y """
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+
+
+
 def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
     """ Split les data en train set et test set """
     if shuffle:
@@ -14,4 +26,6 @@ def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
     y_train, y_test = y[:split_i], y[split_i:]
 
     return X_train, X_test, y_train, y_test
+
+
 
