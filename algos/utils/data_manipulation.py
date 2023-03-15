3adfcfa2ca6419b3670074fe1bc620e6b54fc6ba
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-15 15:24:49 +0100

    add preprocessing

--- algos/utils/data_manipulation.py
+++ algos/utils/data_manipulation.py
@@ -29,3 +29,23 @@ def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
 
 
 
+def normalize(X, axis=-1, order=2):
+    """ Normalise le dataset X """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
+
+
+
+def standardize(X):
+    """ Standardise le dataset X """
+    X_std = X
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    for col in range(np.shape(X)[1]):
+        if std[col]:
+            X_std[:, col] = (X_std[:, col] - mean[col]) / std[col]
+    # X_std = (X - X.mean(axis=0)) / X.std(axis=0)
+    return X_std
+
+
