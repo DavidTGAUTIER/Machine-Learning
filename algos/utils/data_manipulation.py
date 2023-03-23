f59d628b6c8bfe9c325bcbc4dfea47806eaf09c4
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-23 15:39:51 +0100

    add bayes_reg

--- algos/utils/data_manipulation.py
+++ algos/utils/data_manipulation.py
@@ -49,3 +49,11 @@ def standardize(X):
     return X_std
 
 
+
+def polynomial_features(X, degree):
+    n_samples, n_features = np.shape(X)
+
+    def index_combinations():
+        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree+1)]
+        flat_combs = [item for sublist in combs for item in sublist]
+        return flat_combs
