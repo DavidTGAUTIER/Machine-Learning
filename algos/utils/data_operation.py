198b6ba0588de2c9395179a5df7a5b047c4eeff3
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-25 16:17:44 +0100

    add file

--- algos/utils/data_operation.py
+++ algos/utils/data_operation.py
@@ -53,10 +53,20 @@ def calculate_entropy(y):
     log2 = lambda x: math.log(x) / math.log(2)
     unique_labels = np.unique(y)
     entropy = 0
-    
+
     for label in unique_labels:
         count = len(y[y == label])
         p = count / len(y)
         entropy += -p * log2(p)
 
     return entropy
+
+
+
+def calculate_variance(X):
+    """ Retourne la variance des features du dataset X """
+    mean = np.ones(np.shape(X)) * X.mean(0)
+    n_samples = np.shape(X)[0]
+    variance = (1 / n_samples) * np.diag((X - mean).T.dot(X - mean))
+    
+    return variance
