6dbaebd410f77e9da25c018e1f4683313d594611
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-25 15:37:11 +0100

    add file

--- algos/utils/data_manipulation.py
+++ algos/utils/data_manipulation.py
@@ -66,3 +66,24 @@ def polynomial_features(X, degree):
         X_new[:, i] = np.prod(X[:, index_combs], axis=1)
 
     return X_new
+
+
+
+def divide_on_feature(X, feature_i, threshold):
+    """  Diviser le dataset si la valeur du sample de l'index
+         des features est supérieure au seuil donné"""
+    split_func = None
+    
+    if isinstance(threshold, int) or isinstance(threshold, float):
+        split_func = lambda sample: sample[feature_i] >= threshold
+    else:
+        split_func = lambda sample: sample[feature_i] == threshold
+
+    X_1 = np.array([sample for sample in X if split_func(sample)])
+    X_2 = np.array([sample for sample in X if not split_func(sample)])
+
+    return np.array([X_1, X_2])
+
+# divide_on_feature
+
+# mean_square_error
